8be4b91b8b347c8604eb12087c9f1e90c745596b
kwondang <qaz8461@naver.com>
2019-12-29 23:22:59 +0900

    first _ clopn e _ commit

--- test2.py
+++ test2.py
@@ -7,3 +7,11 @@ print('test')
 print('test')
 print('test')
 print('브랜치 test')
+
+flkdsnlfksdmkljsad
+
+lfjknsdmkllfjknsdmkl
+lfjknsdmkl
+lfjknsdmkl
+lfjknsdmkl
+lfjknsdmkllfjknsdmkllfjknsdmkllfjknsdmkl
