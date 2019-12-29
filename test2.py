3d34c737a1a701a1a8864fccf7d2b6b428d0884e
kwondang <qaz8461@naver.com>
2019-12-29 23:25:56 +0900

    마스터 변경

--- test2.py
+++ test2.py
@@ -2,16 +2,3 @@ print('test')
 print('test')
 print('test')
 print('test')
-
-print('test')
-print('test')
-print('test')
-print('브랜치 test')
-
-flkdsnlfksdmkljsad
-
-lfjknsdmkllfjknsdmkl
-lfjknsdmkl
-lfjknsdmkl
-lfjknsdmkl
-lfjknsdmkllfjknsdmkllfjknsdmkllfjknsdmkl
